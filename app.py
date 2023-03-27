a06c68138f30f1f529af063e95d9eacf9194e591
Tushar Rao <tushar.rao198@gmail.com>
2023-03-27 17:14:14 +0530

    feat: error handling

--- app.py
+++ app.py
@@ -14,59 +14,73 @@ class VergeScraper:
         r = requests.get(self.base_url)
         soup = BeautifulSoup(r.text, "html.parser")
         divdata = soup.find('div', "h-full w-full lg:max-h-full lg:w-[380px] lg:pt-[174px]")
-        articles = divdata.find("ol")
-        for article in articles:
-            url = article.find("a")["href"]
-            self.article_urls.append(f"https://www.theverge.com{url}")
+        if divdata is not None:
+            articles = divdata.find("ol")
+            for article in articles:
+                url = article.find("a")["href"]
+                self.article_urls.append(f"https://www.theverge.com{url}")
 
     def scrape_articles(self):
-        for url in self.article_urls:
-            r = requests.get(url)
-            soup = BeautifulSoup(r.text, "html.parser")
-            article_heading = soup.find('h1')
-            
-            # extract article title
-            headline = article_heading.text.strip()
+        if len(self.article_urls)>0:
+            for url in self.article_urls:
+                r = requests.get(url)
+                soup = BeautifulSoup(r.text, "html.parser")
+                article_heading = soup.find('h1')
+                
+                # extract article title
+                headline = article_heading.text.strip()
 
-            #extract authors
-            authors_list = soup.find('div').find('p')
-            authors = ""
-            for author in authors_list:
-                authors = authors.strip() + " " + author.text.strip()
+                #extract authors
+                authors_list = soup.find('div').find('p')
+                authors = ""
+                for author in authors_list:
+                    authors = authors.strip() + " " + author.text.strip()
 
-            # extract date
-            date_str = soup.find('time').text.strip()            
-            article_id = hash(url)
-            article = (int(article_id), url, headline, authors, date_str)
-            print(article)
-            self.article_data.append(article)
+                # extract date
+                date_raw = soup.find('time').text.strip().split(",")
+                date_str = date_raw[0] + date_raw[1]
+
+                # create an tuple object of each article                
+                article_id = hash(url)
+                article = (int(article_id), url, headline, authors, date_str)
+                self.article_data.append(article)
+        else:
+            return "NO ARTICLES FOUND!"
 
     def save_to_csv(self):
         filename = date.today().strftime("%d%m%Y") + "_verge.csv"
-        with open(filename, "w", newline="", encoding="utf-8") as f:
-            writer = csv.writer(f)
-            writer.writerow(["id", "url", "headline", "author", "date"])
-            for article in self.article_data:
-                writer.writerow(article)
+        try:
+            with open(filename, "w", newline="", encoding="utf-8") as f:
+                csvwriter = csv.writer(f)
+                csvwriter.writerow(["id", "url", "headline", "author", "date"])
+                for article in self.article_data:
+                    csvwriter.writerow(article)
 
+        except:
+            return "ERROR SAVING CSV"
+        
     def save_to_sqlite(self):
-        conn = sqlite3.connect("verge_articles.db")
-        c = conn.cursor()
-        c.execute(
-            """
-            CREATE TABLE IF NOT EXISTS articles (
-                id INTEGER PRIMARY KEY,
-                url TEXT,
-                headline TEXT,
-                author TEXT,
-                date TEXT
+        try:
+            conn = sqlite3.connect("verge_articles.db")
+            c = conn.cursor()
+            c.execute(
+                """
+                CREATE TABLE IF NOT EXISTS articles (
+                    id INTEGER PRIMARY KEY,
+                    url TEXT,
+                    headline TEXT,
+                    author TEXT,
+                    date TEXT
+                )
+                """
             )
-            """
-        )
-        for article in self.article_data:
-            c.execute("INSERT OR IGNORE INTO articles VALUES (?, ?, ?, ?, ?)", article)
-        conn.commit()
-        conn.close()
+            for article in self.article_data:
+                c.execute("INSERT OR IGNORE INTO articles VALUES (?, ?, ?, ?, ?)", article)
+            conn.commit()
+            conn.close()
+
+        except:
+            return "ERROR SAVING ARTICLES IN DB"
 
 if __name__ == "__main__":
     scraper = VergeScraper()
